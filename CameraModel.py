2ebd432caeece2de47ed7a065ce4d739749c2122
David W Hogg <david.hogg@nyu.edu>
2012-01-30 23:15:24 -0500

    two-d lens, plus distortions, image

--- CameraModel.py
+++ CameraModel.py
@@ -21,39 +21,61 @@ def intensity(l,a,x,y):
 
 def main():
     lam = 1.2 # wavelength in microns
-    D = 50. * lam # camera aperture in microns
-    f = 200. * lam # focal length in microns
-    K = 2. * D / lam # number of emitting points
+    xres = 0.5 * lam # resolution of antenna grid
+    D = 20. * lam # camera aperture in microns
+    f = 4. * D # focal length in microns
 
-    # set up parabolic lens or mirror
-    x = np.zeros((K,3))
-    x[:,0] = (2. * np.arange(K) - K + 1.) * D / K
-    x[:,1] = 0.5 * x[:,0]**2 / f # all on a parabola
+    # set up perfect parabolic lens or mirror
+    x0, x1 = np.mgrid[0:2 * D / lam + 1,0:2 * D / lam + 1]
+    x0 = (x0.flatten() - np.median(x0)) * xres
+    x1 = (x1.flatten() - np.median(x1)) * xres
+    I = (x0**2 + x1**2) < (0.25 * D**2)
+    x0 = x0[I]
+    x1 = x1[I]
+    K = len(x0)
+    x = np.zeros((K, 3))
+    x[:,0] = x0
+    x[:,1] = x1
+    x[:,2] = 0.5 * (x0**2 + x1**2) / f # all on a parabola
+
+    # distort lens
+    order = 3
+    for o in range(order + 1):
+        for yo in range(o + 1):
+            xo = o - yo
+            x[:,2] += np.random.normal() * (x[:,0] / D)**xo * (x[:,1] / D)**yo
 
     # set up antenna phases
     a = np.ones(K).astype('complex') # all identical
-    # tilt the incoming plane wave by a small angle
-    a *= np.exp(1j * np.linspace(0.,5.,K))
 
     # set up focal plane and put it in focus
-    yres = 0.01 * f * lam / D
-    N = 1000
-    y = np.zeros((N,3))
-    y[:,0] = (np.arange(N) - 0.5 * N + 0.5) * yres
-    y[:,1] = f
+    yres = 0.2 * f * lam / D
+    Ny = 65
+    y0, y1 = np.mgrid[0:Ny, 0:Ny]
+    y0 = (y0.flatten() - np.median(y0)) * yres
+    y1 = (y1.flatten() - np.median(y1)) * yres
+    N = len(y0)
+    y = np.zeros((N, 3))
+    y[:,0] = y0
+    y[:,1] = y1
+    y[:,2] = f
 
     # compute and plot intensity
     I = intensity(lam, a, x, y)
     plt.clf()
-    plt.subplot(2,1,1)
-    for xi, ai in zip(x,a):
-        plt.plot([xi[0], xi[0] - np.imag(ai)], [xi[1], xi[1] + np.real(ai)], 'r-')
-        plt.plot(xi[0], xi[1], 'k.')
+    plt.subplot(2,2,1)
+    plt.plot(x[:,2], x[:,1], 'k.', alpha=0.25)
     plt.axis('equal')
-    plt.xlim(np.min(x[:,0]), np.max(x[:,0]))
-    plt.subplot(2,1,2)
-    plt.plot(y[:,0], I, 'k-')
-    plt.xlim(np.min(y[:,0]), np.max(y[:,0]))
+    plt.subplot(2,2,2)
+    plt.plot(x[:,0], x[:,1], 'k.', alpha=0.25)
+    plt.axis('equal')
+    plt.subplot(2,2,3)
+    plt.plot(x[:,0], x[:,2], 'k.', alpha=0.25)
+    plt.axis('equal')
+    plt.subplot(2,2,4)
+    plt.gray()
+    plt.imshow(I.reshape((Ny, Ny)), vmin=0., vmax=1.)
+    plt.axis('equal')
     plt.savefig('cm.png')
     return None
 
