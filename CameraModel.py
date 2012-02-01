7c57fa1282a6748579d76787bf622769e838a8eb
David W Hogg <david.hogg@nyu.edu>
2012-01-31 22:28:13 -0500

    randomize antenna pattern; other tweaks

--- CameraModel.py
+++ CameraModel.py
@@ -4,38 +4,45 @@ if __name__ == '__main__':
     from matplotlib import rc
     rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
     rc('text', usetex=True)
-    import pylab as plt
 import numpy as np
+import pylab as plt
 
-def field(l,a,x,y):
+def field(lam,a,x,y):
     '''
     Given a set of K monochromatic sources emitting radiation at
-    wavelength l from K three-dimensional positions x[:] with K
+    wavelength lam from K three-dimensional positions x[:] with K
     complex amplitudes a[:], compute N complex amplitudes b[:] at N
     three-dimensional positions y[:], assuming no reflections or
     absorptions anywhere (that is, vacuum).
     '''
     dist = np.sqrt(np.sum((x[np.newaxis,:,:] - y[:,np.newaxis,:])**2, axis=2))
-    return np.sum((a[np.newaxis,:] / dist) * np.exp(2. * np.pi * 1j * dist / l), axis=1) 
+    return np.sum((a[np.newaxis,:] / dist) * np.exp(2. * np.pi * 1j * dist / lam), axis=1) 
 
-def intensity(l,a,x,y):
+def intensity(lam,a,x,y):
     '''
     Same as field(l,a,x,y) but returning N intensities I[:] at the N
     three-dimensional positions y[:].
     '''
-    b = field(l,a,x,y)
+    b = field(lam,a,x,y)
     return np.real(b)**2 + np.imag(b)**2
 
-def parabolic_mirror(D, f, xres):
+def parabolic_mirror(D, f, xres, random=False):
     '''
     Return a set of three-dimensional points x[:] that constitute a
     mirror of radius D and focal length f centered at (0,0,0) and
-    centered and aligned along the z (x[:,2]) axis.  Set the (regular)
-    spacing between points in the x-y plane to xres.
+    centered and aligned along the z (x[:,2]) axis.  Set the (regular,
+    square-grid) spacing between points in the x-y plane to xres.
+
+    If random, then do the same but not on a regular square grid.
     '''
-    x0, x1 = np.mgrid[0:D / xres + 2,0:D / xres + 2]
-    x0 = (x0.flatten() - np.median(x0)) * xres
-    x1 = (x1.flatten() - np.median(x1)) * xres
+    if random:
+        Ntmp = np.ceil(D / xres).astype(int)**2
+        x0 = np.random.uniform(-0.5 * D, 0.5 * D, Ntmp)
+        x1 = np.random.uniform(-0.5 * D, 0.5 * D, Ntmp)
+    else:
+        x0, x1 = np.mgrid[0:D / xres + 2,0:D / xres + 2]
+        x0 = (x0.flatten() - np.median(x0)) * xres
+        x1 = (x1.flatten() - np.median(x1)) * xres
     I = (x0**2 + x1**2) < (0.25 * D**2)
     x0 = x0[I]
     x1 = x1[I]
@@ -93,7 +100,9 @@ def plot_lens_and_intensity(x,y,I):
     plt.axis('equal')
     plt.subplot(2,2,4)
     plt.gray()
-    plt.imshow(np.log(I.reshape((Ny, Ny))), vmin=-6., vmax=4.)
+    logI = np.log(I.reshape((Ny, Ny)))
+    vmax = np.max(logI)
+    plt.imshow(logI, vmin=vmax-8., vmax=vmax)
     plt.axis('equal')
     return None
 
@@ -101,7 +110,7 @@ def main():
     lam = 1.2 # wavelength in microns
     D = 20. * lam # camera aperture in microns
     f = 4. * D # focal length in microns
-    xp = parabolic_mirror(D, f, 0.5 * lam)
+    xp = parabolic_mirror(D, f, 0.5 * lam, random=True)
     x = distort_mirror(xp)
     K = len(x)
     a = np.ones(K).astype('complex') # all identical phases to start
