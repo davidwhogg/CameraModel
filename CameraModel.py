fb84652d7160ee3dbcd56e3aacb57ff42a31a7cb
David W. Hogg <dwhogg@gmail.com>
2012-03-06 22:14:44 -0500

    cleaned up comments and README to current Hogg style

--- CameraModel.py
+++ CameraModel.py
@@ -15,15 +15,17 @@ import pylab as plt
 
 class OpticalSurface():
     '''
+    ## class `OpticalSurface`:
+
     A surface of a lens or a mirror or a wavefront.
 
-    Input at initialization: the diameter D of the (circular) surface
-    and the resolution xres (really the grid spacing).  At
+    Input at initialization: the diameter `D` of the (circular)
+    surface and the resolution `xres` (really the grid spacing).  At
     initialization, the surface is centered on the origin and normal
-    to the z (x[:,2]) axis.
+    to the z (`x[:,2]`) axis.
     
-    Optional input at initialization: If square, then make the surface
-    square not circular.
+    Optional input at initialization: If `square`, then make the
+    surface square not circular.
     '''
     def __init__(self, D, xres, square=False):
         self.D = D
@@ -43,6 +45,11 @@ class OpticalSurface():
         return None
 
     def copy(self):
+        '''
+        ## `copy()`:
+
+        Duh.
+        '''
         other = OpticalSurface(0., 1.)
         other.D = self.D
         other.xres = self.xres
@@ -52,33 +59,41 @@ class OpticalSurface():
 
     def shift(self, deltaz):
         '''
-        Move the optical surface in the z (x[:,2]) direction by
-        distance deltaz.
+        ## `shift()`:
+
+        Move the optical surface in the z (`x[:,2]`) direction by
+        distance `deltaz`.
         '''
         self.x[:,2] += deltaz
         return None
 
     def distort_parabolically(self, f):
         '''
+        ## `distort_parabolically()`:
+
         Distort the optical surface from a flat plane into a mirror of
-        focal length f.  This routine assumes that the surface is
-        centered and aligned along the z (x[:,2]) axis.  Positive and
-        negative f correspond to the same focal length but different
-        orientations; RTFSC.
+        focal length `f`.  This routine assumes that the surface is
+        centered and aligned along the z (`x[:,2]`) axis.  Positive
+        and negative `f` correspond to the same focal length but
+        different orientations; RTFSC.
         '''
         self.x[:,2] += 0.25 * (self.x[:,0]**2 + self.x[:,1]**2) / f
         return None
 
     def distort_randomly(self, amp=0.06, startorder=10, endorder=15):
         '''
-        Shift the surface in the z direction by random amounts
-        according to a hard-coded and insane set of rules.
+        ## `distort_randomly()`:
 
-        Bugs: There is almost certainly a chebyshev-based one-liner
-        that can be done here, but I can't figure out the chebyshev
-        coefficient formatting for two-d chebyshev functions.  And the
-        numpy documentation I see online is inconsistent with what I
-        get locally on my computer; not sure what's up.
+        Shift the surface in the z (`x[:,2]`) direction by random
+        amounts according to a hard-coded and insane set of rules.
+
+        # bugs:
+
+        There is almost certainly a chebyshev-based one-liner that can
+        be done here, but I can't figure out the chebyshev coefficient
+        formatting for two-d chebyshev functions.  And the numpy
+        documentation I see online is inconsistent with what I get
+        locally on my computer; not sure what's up.
         '''
         xx = 2. * self.x[:,0] / self.D # -1 < xx < 1
         yy = 2. * self.x[:,1] / self.D # -1 < yy < 1
@@ -92,8 +107,10 @@ class OpticalSurface():
 
     def drill_hole(self, r):
         '''
-        Remove surface points within a circular region of radius r at
-        the center.
+        ## `drill_hole()`:
+
+        Remove surface points within a circular region of radius `r`
+        at the center.
         '''
         I = (self.x[:,0]**2 + self.x[:,1]**2) > (r**2)
         self.x = self.x[I]
@@ -101,23 +118,30 @@ class OpticalSurface():
 
     def field(self, lam, a, transmitter):
         '''
-        Given a transmitter OpticalSurface emitting radiation at
-        wavelength lam with complex amplitude map a[:], compute and
-        return complex amplitudes map at the receiver surface (self)
-        assuming no reflections or absorptions anywhere (that is,
-        vacuum).
+        ## `field()`:
+
+        Given a `transmitter`, which is an `OpticalSurface`, emitting
+        radiation at wavelength `lam` with complex amplitude map
+        `a[:]`, compute and return complex amplitudes map at the
+        receiver surface (self) assuming no reflections or absorptions
+        anywhere (that is, vacuum).
         '''
         dist = np.sqrt(np.sum((transmitter.x[:,np.newaxis,:] - self.x[np.newaxis,:,:])**2, axis=2))
         return np.sum((a[:,np.newaxis] / dist) * np.exp(2. * np.pi * 1j * dist / lam), axis=0)
 
 class CameraStage():
     '''
-    A stage in a CameraModel made up of a transmitter OpticalSurface
-    and a receiver OpticalSurface.  See classes OpticalSurface() and
-    Camera() for more context and some detail.
+    ## class `CameraStage`:
 
-    Input at initialization: A transmitter surface (of class
-    OpticalSurface) and a receiver surface (same).
+    A stage in a `CameraModel` made up of a transmitter
+    `OpticalSurface` and a receiver `OpticalSurface`.  See classes
+    `OpticalSurface()` and `Camera()` for more context and some
+    detail.
+
+    # initialization input:
+
+    * `transmitter`: An `OpticalSurface`.
+    * `receiver`: An `OpticalSurface`.
     '''
     def __init__(self, transmitter, receiver):
         self.transmitter = transmitter.copy()
@@ -126,28 +150,42 @@ class CameraStage():
 
     def receive(self, lam, amplitude):
         '''
-        Input: the wavelength lambda at which we are working, and the
-        specification of the complex amplitude map at the transmitter
-        surface
+        ## `receive()`:
 
-        Output: the complex amplitude map at the receiver surface
+        Perform the Green function convolution of the complex
+        `amplitude` on the `transmitter` to get the complex amplitude
+        on the `receiver`.
+
+        # input:
+
+        * `lam`: the wavelength lambda at which we are working.
+        * `amplitude`: the complex amplitude map at the transmitter
+          surface.
+
+        # output:
+
+        The complex amplitude map at the receiver surface.
         '''
         return self.receiver.field(lam, amplitude, self.transmitter)
 
 class Camera():
     '''
-    The camera model after which this project is named is made up of a
-    set of CameraStages.  In each CameraStage, there is a transmitter
-    OpticalSurface that is broadcasting wavefronts and a receiver
-    OpticalSurface that is detecting them.  In a Camera with multiple
-    CameraStages, the properties of the transmitter OpticalSurface are
-    set by the receiver OpticalSurface from the previous CameraStage,
-    and the information obtained by the receiver OpticalSurface is
-    used to set the transmitter OpticalSurface of the next
-    CameraStage.  This might be an idiotic methodology.
+    ## `Camera`:
 
-    Input at initialization: a list of stages (iterable of
-    CameraStage elements)
+    The camera model after which this project is named is made up of a
+    set of `CameraStage`s.  In each `CameraStage`, there is a
+    transmitter `OpticalSurface` that is broadcasting wavefronts and a
+    receiver `OpticalSurface` that is detecting them (via `receive()`
+    functions, which are convolutions).  In a `Camera` with multiple
+    `CameraStages`, the properties of the transmitter `OpticalSurface`
+    are set by the receiver `OpticalSurface` from the previous
+    `CameraStage`, and the information obtained by the receiver
+    `OpticalSurface` is used to set the transmitter `OpticalSurface`
+    of the next `CameraStage`.  This might be an idiotic methodology.
+
+    # initialization input:
+
+    * `stages`: A list (iterable) of `CameraStage` elements.
     '''
     def __init__(self, stages):
         self.stages = stages
@@ -155,12 +193,22 @@ class Camera():
 
     def take_one_image(self, lam, amplitude=None):
         '''
-        Input: the wavelength lam at which the image is to be taken
-        and the specification of the complex amplitude map at the
-        transmitter surface of the zeroth stage
+        ## `take_one_image()`:
 
-        Output: the intensity at the receiver surface of the final
-        stage
+        Transmit an input complex amplitude map through the whole
+        `Camera`.
+
+        # input:
+
+        * `lam`: The wavelength lam at which the image is to be taken.
+        * `amplitude`: The complex amplitude map at the transmitter
+          surface of the zeroth `CameraStage`.  If `None` then just
+          make a constant-amplitude map.
+
+        # output:
+
+        The intensity (not amplitude!) at the receiver surface of the
+        final `CameraStage`.
         '''
         if amplitude is None:
             amp = np.ones(len(self.stages[0].transmitter.x))
@@ -174,13 +222,15 @@ class Camera():
 
 class Coronograph(Camera):
     '''
-    A particular kind of Camera that has the properties of a
-    coronograph.  The zeroth CameraStage has a parabolic incoming
+    ## class `Coronograph`:
+
+    A particular kind of `Camera` that has the properties of a
+    coronograph.  The zeroth `CameraStage` has a parabolic incoming
     wavefront transmitter, converging on a focal plane receiver with a
-    hole drilled in it.  The first CameraStage makes that focal plane
-    the transmitter back to an identical parabolic reflector receiver.
-    The third has that reflector transmit back to the final focal
-    plane.
+    hole drilled in it.  The first `CameraStage` makes that focal
+    plane the transmitter back to an identical parabolic reflector
+    receiver.  The third has that reflector transmit back to the final
+    focal plane.  RTFSC.
     '''
     def __init__(self, fratio=4, distorted=True):
         self.stages = []
@@ -203,8 +253,14 @@ class Coronograph(Camera):
         return None
 
 def main():
-    cg = Coronograph()
-    for lam in np.arange(1.0, 2.01, 0.02):
+    '''
+    For command-line operation, to show stuff to Leslie Greengard
+    (NYU).
+    '''
+    np.random.seed(42)
+    lam = 1.5
+    for distorted in (False, True):
+        cg = Coronograph(distorted=distorted)
         image = cg.take_one_image(lam)
         Ny = np.round(np.sqrt(len(image))).astype(int)
         logI = np.log(image.reshape((Ny, Ny)))
@@ -213,7 +269,11 @@ def main():
         plt.gray()
         plt.imshow(logI, vmin=vmax-8., vmax=vmax)
         plt.axis('equal')
-        plt.savefig('cm%3.2f.png' % lam)
+        if distorted:
+            suffix = 'distorted'
+        else:
+            suffix = 'undistorted'
+        plt.savefig('coronograph-%3.2f-%s.png' % (lam, suffix))
     return None
 
 if __name__ == '__main__':
