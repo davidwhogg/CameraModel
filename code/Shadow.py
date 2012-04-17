fe27721fd7168dba45291275410ddcc40fcbb5af
David W. Hogg <dwhogg@gmail.com>
2012-04-17 12:00:45 -0400

    start E&M homework problem

---
+++ code/Shadow.py
@@ -0,0 +1,19 @@
+'''
+This file is part of CameraModel.
+Copyright 2012 David W. Hogg (NYU) <http://cosmo.nyu.edu/hogg/>.
+
+Look at a diffractive shadow from a circular opening.
+'''
+
+if __name__ == '__main__':
+    import matplotlib
+    matplotlib.use('Agg')
+    from matplotlib import rc
+    rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
+    rc('text', usetex=True)
+import numpy as np
+import pylab as plt
+from CameraModel import *
+
+if __name__ == '__main__':
+    print "Hello world"
